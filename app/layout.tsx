160f1a336351e987facc3f75d0dd49181aa32a61
szy her <szymonhernik123@gmail.com>
2025-01-17 16:17:06 +0100

    clear some basic styles

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     <html lang="en" className={clsx(zimula.className)} suppressHydrationWarning>
       <body>
         <Providers>
-          <main className="mx-auto max-w-screen-sm overflow-x-hidden px-6 py-24 md:overflow-x-visible ">
+          <main className="px-4 py-4 ">
             <article className="article">{children}</article>
           </main>
         </Providers>
