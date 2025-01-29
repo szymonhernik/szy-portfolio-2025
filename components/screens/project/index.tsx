30bc1d6de98323b5c59fcc552ffeb099ebd15113
szy her <szymonhernik123@gmail.com>
2025-01-29 17:53:36 +0100

    adjust tags size

--- components/screens/project/index.tsx
+++ components/screens/project/index.tsx
@@ -64,7 +64,7 @@ export default function ProjectPage({
             {project.hasSubprojects &&
             project.subprojects &&
             project.subprojects.length > 0 ? (
-              <div className="text-secondary text-sm ">
+              <div className="text-secondary text-base md:text-[.9rem] ">
                 {project.subprojects
                   .flatMap((subproject) => subproject.categories)
                   .filter(
@@ -85,7 +85,7 @@ export default function ProjectPage({
               </div>
             ) : (
               project.categories && (
-                <div className="text-secondary text-sm lg:text-xs">
+                <div className="text-secondary text-[.9rem] lg:text-xs">
                   {project.categories.map((category, index, array) => (
                     <>
                       <Link
