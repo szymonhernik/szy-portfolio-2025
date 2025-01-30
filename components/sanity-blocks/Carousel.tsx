1b93a84b14cb519bf10843761b2dad7b32dc5db3
szy her <szymonhernik123@gmail.com>
2025-01-30 16:18:49 +0100

    add a max width for the blur state in carousel in project page

--- components/sanity-blocks/Carousel.tsx
+++ components/sanity-blocks/Carousel.tsx
@@ -5,6 +5,7 @@ import type { MuxVideoAssetOwn } from "@/types/mux";
 
 import PortableTextRenderer from "@/components/portable-text-renderer";
 import { useCarousel } from "@/contexts/CarouselContext";
+import { decimalToRatio } from "@/lib/calculating";
 
 import useEmblaCarousel from "embla-carousel-react";
 import Image from "next/image";
@@ -12,7 +13,10 @@ import Image from "next/image";
 import { useDotButton } from "../carousel-embla/EmblaCarouselDotButton";
 import MuxPlayerWrapper from "../mux-player-wrapper";
 
-type CarouselBlock = Extract<NonNullable<NonNullable<SingleProjectQueryResult>["blocks"]>[number], { _type: "carousel" }> & {
+type CarouselBlock = Extract<
+  NonNullable<NonNullable<SingleProjectQueryResult>["blocks"]>[number],
+  { _type: "carousel" }
+> & {
   defaultCaption?: string;
 };
 
@@ -28,7 +32,10 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
     if (!items?.[0]) return "3 / 2"; // default fallback
     const firstSlide = items[0];
 
-    if ("image" in firstSlide && firstSlide.image?.asset?.metadata?.dimensions) {
+    if (
+      "image" in firstSlide &&
+      firstSlide.image?.asset?.metadata?.dimensions
+    ) {
       const { width, height } = firstSlide.image.asset.metadata.dimensions;
       return `${width} / ${height}`;
     }
@@ -49,8 +56,21 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
     if (!slide) return null;
 
     if ("image" in slide) {
+      const aspectRatio = slide.image?.asset?.metadata?.dimensions?.aspectRatio
+        ? decimalToRatio(slide.image.asset.metadata.dimensions.aspectRatio)
+        : "16/9";
       return (
-        <div className="relative h-full ">
+        <div
+          className="relative h-full "
+          style={{
+            aspectRatio: aspectRatio,
+            maxWidth: `calc(90vh * ${aspectRatio
+              .split("/")
+              .map(Number)
+              .reduce((a, b) => a / b)
+              .toString()})`,
+          }}
+        >
           <Image
             src={slide.image?.asset?.url || ""}
             alt={slide.image?.alt || ""}
@@ -69,7 +89,10 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
       );
     }
 
-    if ("video" in slide && (slide.video?.asset as unknown as MuxVideoAssetOwn)?.playbackId) {
+    if (
+      "video" in slide &&
+      (slide.video?.asset as unknown as MuxVideoAssetOwn)?.playbackId
+    ) {
       return (
         <div className="relative h-full w-full">
           <MuxPlayerWrapper
@@ -105,14 +128,20 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
   const getGlobalIndex = (localIndex: number) => {
     if (!items?.[0]?._key || !allSlides) return localIndex;
     const firstSlideKey = items[0]._key;
-    const globalStartIndex = allSlides.findIndex((slide) => slide._key === firstSlideKey);
+    const globalStartIndex = allSlides.findIndex(
+      (slide) => slide._key === firstSlideKey,
+    );
     return globalStartIndex + localIndex;
   };
 
   return (
     <div className="relative my-4 w-full">
       <div className="embla flex flex-col gap-2">
-        <div className="embla__viewport" ref={emblaRef} style={{ aspectRatio: getAspectRatio() }}>
+        <div
+          className="embla__viewport"
+          ref={emblaRef}
+          style={{ aspectRatio: getAspectRatio() }}
+        >
           <div className="embla__container ">
             {items.map((slide, index) => (
               <div key={slide._key} className="embla__slide overflow-hidden">
@@ -122,7 +151,13 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
           </div>
         </div>
         <div className="flex justify-between text-small lg:text-xs">
-          <button className="text-secondary" type="button" onClick={() => openFullScreen(allSlides, getGlobalIndex(selectedIndex))}>
+          <button
+            className="text-secondary"
+            type="button"
+            onClick={() =>
+              openFullScreen(allSlides, getGlobalIndex(selectedIndex))
+            }
+          >
             Click to view full-screen
           </button>
           <span className="">{`${selectedIndex + 1}–${items.length}`}</span>
