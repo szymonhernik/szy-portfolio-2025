76e1b38ea750822bd7082a2259d0d1b2a1843ac5
szy her <szymonhernik123@gmail.com>
2025-01-31 20:17:27 +0100

    small styling

--- components/screens/home/index.tsx
+++ components/screens/home/index.tsx
@@ -13,7 +13,10 @@ function IntroText() {
       <br className="block md:hidden" />
       <span className="block md:inline ">
         They also form half of{" "}
-        <Link href="/iszszistudio" className="text-link hover:font-outline-1-secondary">
+        <Link
+          href="/iszszistudio"
+          className="text-link hover:font-outline-1-secondary"
+        >
           isz szi studio
         </Link>
         , an artistic design studio based in Brussels.
@@ -25,7 +28,10 @@ function ListSeed() {
   return (
     <p className="mt-0 ">
       One random inspiration from a rather{" "}
-      <Link href="/garden" className="text-secondary hover:font-outline-1-secondary">
+      <Link
+        href="/garden"
+        className="text-secondary hover:font-outline-1-secondary"
+      >
         unsorted list
       </Link>{" "}
       is: <OpenGardenItem slug="27-30-covers">27 30 @ Covers</OpenGardenItem>
@@ -46,7 +52,7 @@ export default function Home({ projects }: { projects: ProjectQueryResult }) {
         {/* </FadeIn.Item> */}
       </div>
 
-      <div className="col-span-12 mt-8">
+      <div className="col-span-12 mt-24">
         {/* <FadeIn.Item> */}
         <HomeGrid projects={projects} />
         {/* </FadeIn.Item> */}
--- components/screens/iszszistudio/index.tsx
+++ components/screens/iszszistudio/index.tsx
@@ -3,32 +3,48 @@ import Link from "next/link";
 
 export function IszSziStudioContent() {
   return (
-    <section className="flex min-h-[calc(100dvh-2rem)] w-full flex-col justify-between">
+    <section className="flex min-h-[calc(100dvh-2rem)] w-full flex-col justify-between overflow-y-auto">
       <div className="grid grid-cols-12 ">
         <div className="col-span-12 md:col-span-10 lg:col-span-7">
           <p>
-            <Link href="https://iszszistudio.com">isz szi</Link> is an ambivalently focused studio that continues to find different ways of expression and
-            labour.
+            <Link href="https://iszszistudio.com">isz szi</Link> is an
+            ambivalently focused studio that continues to find different ways of
+            expression and labour.
           </p>
           <p>
-            In our work we always try to reflect on what's structurally at stake. We think a lot about what it means and how to collaborate, and how to sustain
-            an ethics of love in times of austerity. We remain impressed by oeuvres such as Frost&Lynch's Twin Peaks series or the newest publishing venture in
-            Poland called Współbycie.
+            In our work we always try to reflect on what's structurally at
+            stake. We think a lot about what it means and how to collaborate,
+            and how to sustain an ethics of love in times of austerity. We
+            remain impressed by oeuvres such as Frost&Lynch's Twin Peaks series
+            or the newest publishing venture in Poland called Współbycie.
           </p>
           <p>
             Our most recent projects include a website for Polish musician{" "}
-            <Link target="_blank" href="https://zaumne.com" className="text-link hover:font-outline-1-secondary">
+            <Link
+              target="_blank"
+              href="https://zaumne.com"
+              className="text-link hover:font-outline-1-secondary"
+            >
               Zaumne
             </Link>
             , a revival of our substack newsletter called{" "}
-            <Link target="_blank" href="https://iszszistudio.substack.com" className="text-link hover:font-outline-1-secondary">
+            <Link
+              target="_blank"
+              href="https://iszszistudio.substack.com"
+              className="text-link hover:font-outline-1-secondary"
+            >
               branches
             </Link>{" "}
             and motion design for the{" "}
-            <Link target="_blank" href="https://www.instagram.com/p/CvepmI3otWU/" className="text-link hover:font-outline-1-secondary">
+            <Link
+              target="_blank"
+              href="https://www.instagram.com/p/CvepmI3otWU/"
+              className="text-link hover:font-outline-1-secondary"
+            >
               Sanatorium of Sound
             </Link>
-            . What has been inspiring us lately are songs, dogs and throwing balls.
+            . What has been inspiring us lately are songs, dogs and throwing
+            balls.
           </p>
         </div>
       </div>
@@ -45,7 +61,11 @@ export function IszSziStudioContent() {
         </div>
         <p className="mt-4 text-small">
           isz szi studio is the combined force of{" "}
-          <Link target="_blank" href="https://www.instagram.com/ireee_nka/" className="text-link hover:font-outline-1-secondary">
+          <Link
+            target="_blank"
+            href="https://www.instagram.com/ireee_nka/"
+            className="text-link hover:font-outline-1-secondary"
+          >
             ire
           </Link>{" "}
           (the bird looking gal) and Szymon Eda Hernik.
