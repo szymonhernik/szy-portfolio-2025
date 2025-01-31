339cfe59a0a25ebf07a9f19108970452a3045f83
szy her <szymonhernik123@gmail.com>
2025-01-31 18:43:05 +0100

    add aspect ratio for videos in carousel

--- components/sanity-blocks/Carousel.tsx
+++ components/sanity-blocks/Carousel.tsx
@@ -13,7 +13,10 @@ import Image from "next/image";
 import { useDotButton } from "../carousel-embla/EmblaCarouselDotButton";
 import MuxPlayerWrapper from "../mux-player-wrapper";
 
-type CarouselBlock = Extract<NonNullable<NonNullable<SingleProjectQueryResult>["blocks"]>[number], { _type: "carousel" }> & {
+type CarouselBlock = Extract<
+  NonNullable<NonNullable<SingleProjectQueryResult>["blocks"]>[number],
+  { _type: "carousel" }
+> & {
   defaultCaption?: string;
 };
 
@@ -29,13 +32,23 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
     if (!items?.[0]) return "3 / 2"; // default fallback
     const firstSlide = items[0];
 
-    if ("image" in firstSlide && firstSlide.image?.asset?.metadata?.dimensions) {
+    if (
+      "image" in firstSlide &&
+      firstSlide.image?.asset?.metadata?.dimensions
+    ) {
       const { width, height } = firstSlide.image.asset.metadata.dimensions;
       return `${width} / ${height}`;
     }
 
     if ("video" in firstSlide) {
-      return "16 / 9"; // default video aspect ratio
+      console.log(firstSlide.video);
+      // replace : with /
+      // @ts-ignore
+      const aspectRatio = firstSlide.video?.asset?.aspectRatio
+        ? // @ts-ignore
+          firstSlide.video.asset.aspectRatio.replace(":", " / ")
+        : "16/9";
+      return aspectRatio;
     }
 
     return "3 / 2"; // fallback for content slides
@@ -50,7 +63,9 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
     if (!slide) return null;
 
     if ("image" in slide) {
-      const aspectRatio = slide.image?.asset?.metadata?.dimensions?.aspectRatio ? decimalToRatio(slide.image.asset.metadata.dimensions.aspectRatio) : "16/9";
+      const aspectRatio = slide.image?.asset?.metadata?.dimensions?.aspectRatio
+        ? decimalToRatio(slide.image.asset.metadata.dimensions.aspectRatio)
+        : "16/9";
       return (
         <div
           className="relative h-full "
@@ -81,7 +96,10 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
       );
     }
 
-    if ("video" in slide && (slide.video?.asset as unknown as MuxVideoAssetOwn)?.playbackId) {
+    if (
+      "video" in slide &&
+      (slide.video?.asset as unknown as MuxVideoAssetOwn)?.playbackId
+    ) {
       return (
         <div className="relative h-full w-full">
           <MuxPlayerWrapper
@@ -117,14 +135,20 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
   const getGlobalIndex = (localIndex: number) => {
     if (!items?.[0]?._key || !allSlides) return localIndex;
     const firstSlideKey = items[0]._key;
-    const globalStartIndex = allSlides.findIndex((slide) => slide._key === firstSlideKey);
+    const globalStartIndex = allSlides.findIndex(
+      (slide) => slide._key === firstSlideKey,
+    );
     return globalStartIndex + localIndex;
   };
 
   return (
     <div className="relative my-4 w-full">
       <div className="embla flex flex-col gap-2">
-        <div className="embla__viewport" ref={emblaRef} style={{ aspectRatio: getAspectRatio() }}>
+        <div
+          className="embla__viewport"
+          ref={emblaRef}
+          style={{ aspectRatio: getAspectRatio() }}
+        >
           <div className="embla__container ">
             {items.map((slide, index) => (
               <div key={slide._key} className="embla__slide overflow-hidden">
@@ -134,7 +158,13 @@ export default function Carousel({ defaultCaption, items }: CarouselBlock) {
           </div>
         </div>
         <div className="flex justify-between text-small lg:text-xs">
-          <button className="text-secondary" type="button" onClick={() => openFullScreen(allSlides, getGlobalIndex(selectedIndex))}>
+          <button
+            className="text-secondary"
+            type="button"
+            onClick={() =>
+              openFullScreen(allSlides, getGlobalIndex(selectedIndex))
+            }
+          >
             Click to view full-screen
           </button>
           <span className="">{`${selectedIndex + 1}–${items.length}`}</span>
