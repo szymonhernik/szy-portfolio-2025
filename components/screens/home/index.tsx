1d218016275affab4572a77e77262a4d3801dae6
szy her <szymonhernik123@gmail.com>
2025-02-09 17:06:09 +0100

    apply the extra padding on the whole paragraph text in projects

--- components/screens/home/index.tsx
+++ components/screens/home/index.tsx
@@ -7,8 +7,8 @@ import Link from "next/link";
 
 function IntroText() {
   return (
-    <p className="text-fluid-xl ">
-      <span className=" block max-md:pr-4">Szymon Eda Hernik is a graphic designer and front-end web developer.</span>
+    <p className="text-fluid-xl max-md:pr-4">
+      Szymon Eda Hernik is a graphic designer and front-end web developer.
       <br className="block md:hidden" />
       <br className="block md:hidden" />
       <span className="block md:inline ">
