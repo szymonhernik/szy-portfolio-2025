34135ac5c84bcd6436dbbb7833783e25eafb58ee
szy her <szymonhernik123@gmail.com>
2025-02-10 14:11:36 +0100

    resize animation for mobile

--- app/(web)/_components/RandomAnimation.tsx
+++ app/(web)/_components/RandomAnimation.tsx
@@ -21,7 +21,9 @@ export default function RandomAnimation({
 
   // biome-ignore lint/correctness/useExhaustiveDependencies: intentionally changing video on path change
   useEffect(() => {
-    setRandomVideo(videoLibrary[Math.floor(Math.random() * videoLibrary.length)] as Video);
+    setRandomVideo(
+      videoLibrary[Math.floor(Math.random() * videoLibrary.length)] as Video,
+    );
   }, [pathname]);
 
   // Don't render if paths don't match
@@ -35,13 +37,24 @@ export default function RandomAnimation({
     left: `${Math.floor(Math.random() * 70)}vw`,
   };
 
-  const videoSize = randomVideo.size === "lightweight" ? "max-w-[240px] max-h-[240px]" : "max-w-[140px] max-h-[140px]";
+  const videoSize =
+    randomVideo.size === "lightweight"
+      ? "max-w-[240px] max-h-[240px]"
+      : "md:max-w-[140px] md:max-h-[140px] max-w-[90px] max-h-[90px]";
 
   console.log("randomVideo", randomVideo);
 
   return (
-    <div className="absolute top-0 left-0 z-[0] mix-blend-darken w-screen h-screen max-w-screen max-h-screen overflow-hidden" >
-      <video src={randomVideo.url} playsInline autoPlay muted controls={false} className={`${videoSize} absolute `} style={randomPosition} />
+    <div className="absolute top-0 left-0 z-[0] mix-blend-darken w-screen h-screen max-w-screen max-h-screen overflow-hidden">
+      <video
+        src={randomVideo.url}
+        playsInline
+        autoPlay
+        muted
+        controls={false}
+        className={`${videoSize} absolute `}
+        style={randomPosition}
+      />
     </div>
   );
 }
